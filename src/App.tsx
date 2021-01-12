f2c34cff4b74fbaa97dcf754d99c5b2d8d0f170e
hector-crean <hectorcrean@gmail.com>
2021-01-12 12:17:24 +0000

    more-tidying

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { gql, useQuery } from "@apollo/client";
-
-// import { Island } from './types'; 
-
-// import { ApolloConsumer } from '@apollo/client';
-// const WithApolloClient = () => (
-//   <ApolloConsumer>
-//     {client => 'We have access to the client!' /* do stuff here */}
-//   </ApolloConsumer>
-// );
-
 import { StructureAlmere, StructureAlmereArray, decodeWith  } from './types'; 
-import * as D from 'io-ts/Decoder'; 
 
 
 
-const GET_QUERY = gql`
-  query MyQuery {
+const GET_ALMERE_STRUCT = gql`
+  query {
     structureDataAlmere {
       assemblyTimeInDays
       buildingSystem
@@ -39,33 +27,27 @@ const GET_QUERY = gql`
     }
   }`;
 
+/* The magic is happening in types/index.ts file: -> 
+  Creating a StructureAlmere instance using io-ts. We can convert this into a 'decoder' using asDecoder(), and then use the 
+  .decode method to validate our query response. 
+
+*/
 
 
 
 function App() {
 
-  interface Data {structureDataAlmere: StructureAlmere[] }
-
-  const { loading, error, data } = useQuery<Data, StructureAlmere> (GET_QUERY); 
+  type Data = {structureDataAlmere: StructureAlmere[] }
+  type Variables = StructureAlmere
+  
+  const { loading, error, data } = useQuery<Data, Variables> (GET_ALMERE_STRUCT); 
   const decoder = StructureAlmereArray.asDecoder()
   const result = decodeWith(decoder)(data?.structureDataAlmere); 
   console.log(result)
-  // console.log(result); 
-  
-  // const r = TStructureAlmereArray.decode(q.data); 
-  // console.log(r); 
-  
-  // console.log(decodeWith(TStructureAlmereArray.asDecoder())(q.data))
-
-  // const theDecoder: D.Decoder<unknown, StructureAlmereType> = TStructureAlmere.asDecoder()
-  // const r = validator(theDecoder)(q)
-  // console.log('r', r)
-  // console.log(r); 
-
-
+ 
   return (
     <div className="App">
-      {/* <pre>{ JSON.stringify(result, null, 2) }</pre> */}
+      <pre>{ JSON.stringify(result, null, 2) }</pre>
     </div>
   );
 }
